6efba685bec649d42feb95984046b1fa5dac45cd
XB <nguyenxuanbach270901@gmail.com>
2022-10-06 00:02:57 +0700

    live-project-corporate-office-locator

---
+++ resources/data/offices.js
@@ -0,0 +1,38 @@
+var officeList = [
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [72.8746, 19.0793],
+    },
+    properties: {
+      name: "Mumbai",
+      address: "Shop no 2, Ground floor Kanakia Zillion, Mumbai",
+      phone: "22 33 44 55",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [105.804817, 21.028511],
+    },
+    properties: {
+      name: "Ha Noi",
+      address: "Ha Noi, Viet Nam",
+      phone: "22 33 44 55",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-0.118092, 51.509865],
+    },
+    properties: {
+      name: "London",
+      address: "London, England",
+      phone: "22 33 44 55",
+    },
+  },
+];
