6a7d4851bdcca2b564427ac17c45b4d21cdcee8a
XB <nguyenxuanbach270901@gmail.com>
2022-10-05 14:58:59 +0700

    layer switcher wms length measure

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+var osmMap = L.tileLayer.provider("OpenStreetMap.Mapnik");
+var stamenMap = L.tileLayer.provider("Stamen.Terrain");
+var imageryMap = L.tileLayer.provider("Esri.WorldImagery");
 
+var baseMaps = {
+  OSM: osmMap,
+  "Stamen Watorcolor": stamenMap,
+  "World Imagery": imageryMap,
+};
 
+// var geoServerIPPort = "localhost:8080";
+// var geoServerWorkspace = "GIS";
+var stateLayerName = "tiger:tiger_roads";
+
+var tiger = L.tileLayer.wms(
+    "http://localhost:8080/geoserver/tiger/wms",
+    {
+        layers: stateLayerName,
+        format: "image/png",
+        transparent: true,
+        version: "2.21.1",
+        tiled: true,
+    }
+) 
+
+var overlayMaps = {
+    "Tiger" : tiger,
+}
+
+var map = L.map("map", {
+  center: [22.7356, 79.8925],
+  zoom: 5,
+//   layers:[osmMap]
+  layers:[osmMap, tiger]
+});
+
+var mapLayers = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+var ctlMeasure = L.control.polylineMeasure({
+    position: "topleft",
+    measureControlTitle: "Measure Length"
+}).addTo(map)
